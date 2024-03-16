c657b6947bb82dde838c2544b50b601509b21e87
Junjie Chen <junjie.chan@outlook.com>
2024-03-16 23:50:21 +1000

    Article block design: icon + color assortment + font family + text ellipsis

---
+++ web/script.js
@@ -0,0 +1,36 @@
+// // const var=xxx;
+// const p = document.getElementById("test");
+
+// eel.expose;
+// function display(data) {
+//   p.innerText = data["title"];
+//   // eel.<python func>()
+// }
+
+// eel.get_data()(display);
+
+// const b = document.getElementById("btn");
+// b.addEventListener("click", (event) => {
+//   p.innerHTML = "button clicked!";
+//   //   eel.to_second_page();
+//   // redirect
+//   //   window.location.href = "test.html";
+//   //   window.location.href = "https://www.google.com";
+// });
+
+$(document).ready(function () {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function () {
+    /* Check the location of each desired element */
+    $(".reveal").each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it in */
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 1200);
+        /* 1500 is the duration of fade effect */
+      }
+    });
+  });
+});
