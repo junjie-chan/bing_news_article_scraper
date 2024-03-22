d660745c4ba1ce0c761e6e9bbd9546f4c1de6bfc
Junjie Chen <junjie.chan@outlook.com>
2024-03-22 20:23:16 +1000

    Fix Bug: fix the calculation error.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from jinja2 import Environment, FileSystemLoader
 from pandas import DataFrame, set_option
 
 WINDOW_SIZE = (1300, 740)
+ARTICLES_PER_PAGE = 10
 dbm = DatabaseManager()
 
 # 函数放在 .init 和 .start 之间
@@ -26,7 +27,7 @@ def generate_sidebar_code():
 
 
 @expose
-def generate_page_article_blocks(page_no=1, articles_per_page=20):
+def generate_page_article_blocks(page_no=1, articles_per_page=ARTICLES_PER_PAGE):
     articles = DataFrame(dbm.fetch_articles(page_no-1, articles_per_page))
     articles.columns = ['id', 'title', 'url',
                         'description', 'date', 'time', 'keyword']
@@ -36,15 +37,29 @@ def generate_page_article_blocks(page_no=1, articles_per_page=20):
 
 @expose
 def get_maximum_pages():
-    return dbm.get_count()
+    return ceil(dbm.get_count()/ARTICLES_PER_PAGE)
 
 
 @expose
-def generate_page_buttons(articles_per_page=20, starting_page=1):
-    num_of_page = ceil(get_maximum_pages()/articles_per_page)
-    tags = ['Previous', *range(1, num_of_page+1), 'Next'] if num_of_page <= 5 else [
-        'Previous', 1, 2, 3, 'ellipsis', num_of_page-2, num_of_page-1, num_of_page, 'Next']
+def generate_page_buttons(articles_per_page=ARTICLES_PER_PAGE, starting_page=1):
+    num_of_page = get_maximum_pages()
+    tags = ['Previous', *range(1, num_of_page+1), 'Next'] if num_of_page <= 8 else [
+        'Previous', 1, 2, 3, 4, 5, 'ellipsis', num_of_page-2, num_of_page-1, num_of_page, 'Next']
     return run_template('next_page_template.jinja', tags)
 
 
-start('index.html', size=WINDOW_SIZE)
+@expose
+def get_buttons_text(index4_value, max_pages):
+    if index4_value < max_pages-6:
+        if index4_value == 3:
+            return ['Previous', *range(1, 6), '...', *range(max_pages-2, max_pages+1), 'Next']
+        else:
+            return ['Previous', *range(index4_value-2, index4_value), index4_value,
+                    *range(index4_value+1, index4_value+3), '...', *
+                    range(max_pages-2, max_pages+1), 'Next']
+    else:
+        return ['Previous', '...', *range(max_pages-7, max_pages+1), 'Next']
+
+
+# start('index.html', size=WINDOW_SIZE)
+print(get_maximum_pages())
