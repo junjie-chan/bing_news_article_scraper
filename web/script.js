1657d0c4b5d30d956d8c5cdc3a8af7c826929d98
Junjie Chen <junjie.chan@outlook.com>
2024-03-22 17:23:57 +1000

    Auto disable ellipsis button and scroll buttons to right.

--- web/script.js
+++ web/script.js
@@ -116,6 +116,13 @@ async function move_buttons(clicked_button, maximum_pages) {
     } else {
       li_tag.classList.add("page_" + button_text[i]);
     }
+    // 对省略号进行更新：disabled
+    if (li_tag.classList.contains("disabled")) {
+      li_tag.classList.remove("disabled");
+    }
+    if (li_tag.querySelector("a").textContent == "...") {
+      li_tag.classList.add("disabled");
+    }
   }
 }
 
@@ -136,133 +143,143 @@ document.addEventListener("DOMContentLoaded", (event) => {
       var clicked_index = get_element_class_name(clicked_button, "index");
       var clicked_index_int = parseInt(clicked_index.replace("index", ""));
 
-      // 点击已经激活的按钮会没反应
+      // 没有点击已经激活的按钮
       if (clicked_index_int != active_index_int) {
+        // 点击已经激活的按钮会没反应
         var index4 = get_element_by_class_name("index4");
         var index4_page_no = get_page_no(index4);
-        // 第四个按钮为第三页时
-        if (index4_page_no == 3) {
-          // 点击前三页
-          if ([2, 3, 4].includes(clicked_index_int)) {
+
+        // 点击前三页
+        if ([2, 3, 4].includes(clicked_index_int)) {
+          // 第四个按钮为第三页时
+          if (index4_page_no == 3) {
             activate_button(clicked_index_int, active_button);
           }
-          // 点击后两页
-          else if ([5, 6].includes(clicked_index_int)) {
+          // 当去到最后的位置的时候
+          else if (index4_page_no == maximum_pages - 6) {
+          }
+        }
+        // 点击后两页
+        else if ([5, 6].includes(clicked_index_int)) {
+          // 只要还没有到需要去掉省略号的位置
+          if (index4_page_no < maximum_pages - 6) {
             activate_button(4, active_button);
             move_buttons(clicked_button, maximum_pages);
           }
+        }
+        // 第四个按钮为第三页时
+        if (index4_page_no == 3) {
           // 点击最后三页
-          else if ([8, 9, 10].includes(clicked_index_int)) {
-            activate_button(clicked_index_int, active_button);
+          if ([8, 9, 10].includes(clicked_index_int)) {
           }
         }
       }
-
-      // 还没有去到最后9页的时候（第4个按钮小于14）：
-      // if ( && clicked_index_int != 7) {
-      // 需要移动按钮的情况：
-      // 1. 还没有去到最后9页的时候（第4个按钮小于14）：
-      //    可以向左右移动：第4个按钮的页码>3且<14
-      //    只能向右移动：第4个按钮的页码为3
-      // 2. 去到最后9页的时候：
-
-      //   if (
-      //     [4, 6].includes(clicked_index) ||
-      //     (clicked_index_int == 9 && active_index_int == 3) ||
-      //     (clicked_index_int == 1 && active_index_int == 7)
-      //   ) {
-      //   } else {
-      //     // 点击next或previous的情况
-      //     if (clicked_index_int == 1 && get_page_no(active_button) != 1) {
-      //       activate_button(active_index_int - 1, active_button);
-      //     } else if (clicked_index_int == 9) {
-      //     } else {
-      //       activate_button(clicked_index_int, active_button);
-      //       // display_articles(get_page_no(clicked_button));
-      //     }
-      //   }
-      // }
-
-      // var maximum_pages = await eel.get_maximum_pages()();
-      // // No ellipsis
-      // if (maximum_pages <= 5) {
-      // } else {
-      //   // Get the active page
-      //   // var active_page = parseInt(active_button.textContent);
-
-      //   // 更新分页所在位置，如：1,2,3...,5,6，那么如果点击3就应该将其往左移动一格
-      //   // Update li with index 2-8 (exclude Previous & Next), move all li to left or right
-      //   var ul = document.querySelector("div.buttons_container > ul");
-      //   var li_tags = ul.querySelectorAll("li");
-      //   for (let i = 1; i < li_tags.length - 1; i++) {
-      //     // Exclude the ellipsis button
-      //     if (i != 4) {
-      //       var li_tag = li_tags[i];
-      //       if (clicked_index_int == 9 && active_index_int == 3) {
-      //         // Change <li> text
-      //         var page_no_on_tag = parseInt(li_tag.textContent) + 1;
-      //         li_tag.querySelector("a").textContent = page_no_on_tag;
-      //         // Update class name page_
-      //         li_tag.classList.remove(get_element_class_name(li_tag, "page_"));
-      //         li_tag.classList.add("page_" + page_no_on_tag);
-      //       } else if (clicked_index_int == 1 && active_index_int == 7) {
-      //         var page_no_on_tag = parseInt(li_tag.textContent) - 1;
-      //         li_tag.querySelector("a").textContent = page_no_on_tag;
-      //         // Update class name page_
-      //         li_tag.classList.remove(get_element_class_name(li_tag, "page_"));
-      //         li_tag.classList.add("page_" + page_no_on_tag);
-      //       }
-      //     }
-      //   }
-
-      //   // Update active
-      //   if (clicked_button_name == "Next" && active_page < maximum_pages) {
-      //     console.log("active index: " + active_index);
-      //     active_button.classList.remove("active");
-
-      //     // var new_button = document.querySelectorAll(
-      //     //   '[class*="index' + (active_index + 1) + '"]'
-      //     // )[0];
-      //     console.log("index" + (active_index + 1));
-      //     console.log(
-      //       document.getElementsByClassName("index" + (active_index + 1))
-      //     );
-
-      //     // page_to_display = active_page + 1;
-      //   }
-
-      // new_button.classList.add("active");
-
-      // if (active_page != clicked_button_name) {
-      //   // Actions for previous and next buttons & Navigate to page
-      //   if (["Previous", "Next"].includes(clicked_button_name)) {
-      //     // Find out the page to be displayed
-      //     if (clicked_button_name == "Previous" && active_page > 1) {
-      //       page_to_display = active_page - 1;
-      //     } else if (
-      //       clicked_button_name == "Next" &&
-      //       active_page < (await eel.get_maximum_pages()())
-      //     ) {
-      //       page_to_display = active_page + 1;
-      //     } else {
-      //       page_to_display = active_page;
-      //     }
-      //   } else if (active_page != clicked_button_name) {
-      //     page_to_display = clicked_button_name;
-      //   }
-
-      //   if (clicked_button_name != "...") {
-      //     // Remove active from its class list
-      //     active_button.classList.remove("active");
-      //     // Set the next active page and display articles
-      //     var page_to_activate = document.querySelectorAll(
-      //       '[class*="page_' + page_to_display + '"]'
-      //     )[0];
-      //     page_to_activate.classList.add("active");
-      //     display_articles(parseInt(page_to_display));
-      //   }
-      // }
-      // }
     }
   });
 });
+
+// 还没有去到最后9页的时候（第4个按钮小于14）：
+// if ( && clicked_index_int != 7) {
+// 需要移动按钮的情况：
+// 1. 还没有去到最后9页的时候（第4个按钮小于14）：
+//    可以向左右移动：第4个按钮的页码>3且<14
+//    只能向右移动：第4个按钮的页码为3
+// 2. 去到最后9页的时候：
+
+//   if (
+//     [4, 6].includes(clicked_index) ||
+//     (clicked_index_int == 9 && active_index_int == 3) ||
+//     (clicked_index_int == 1 && active_index_int == 7)
+//   ) {
+//   } else {
+//     // 点击next或previous的情况
+//     if (clicked_index_int == 1 && get_page_no(active_button) != 1) {
+//       activate_button(active_index_int - 1, active_button);
+//     } else if (clicked_index_int == 9) {
+//     } else {
+//       activate_button(clicked_index_int, active_button);
+//       // display_articles(get_page_no(clicked_button));
+//     }
+//   }
+// }
+
+// var maximum_pages = await eel.get_maximum_pages()();
+// // No ellipsis
+// if (maximum_pages <= 5) {
+// } else {
+//   // Get the active page
+//   // var active_page = parseInt(active_button.textContent);
+
+//   // 更新分页所在位置，如：1,2,3...,5,6，那么如果点击3就应该将其往左移动一格
+//   // Update li with index 2-8 (exclude Previous & Next), move all li to left or right
+//   var ul = document.querySelector("div.buttons_container > ul");
+//   var li_tags = ul.querySelectorAll("li");
+//   for (let i = 1; i < li_tags.length - 1; i++) {
+//     // Exclude the ellipsis button
+//     if (i != 4) {
+//       var li_tag = li_tags[i];
+//       if (clicked_index_int == 9 && active_index_int == 3) {
+//         // Change <li> text
+//         var page_no_on_tag = parseInt(li_tag.textContent) + 1;
+//         li_tag.querySelector("a").textContent = page_no_on_tag;
+//         // Update class name page_
+//         li_tag.classList.remove(get_element_class_name(li_tag, "page_"));
+//         li_tag.classList.add("page_" + page_no_on_tag);
+//       } else if (clicked_index_int == 1 && active_index_int == 7) {
+//         var page_no_on_tag = parseInt(li_tag.textContent) - 1;
+//         li_tag.querySelector("a").textContent = page_no_on_tag;
+//         // Update class name page_
+//         li_tag.classList.remove(get_element_class_name(li_tag, "page_"));
+//         li_tag.classList.add("page_" + page_no_on_tag);
+//       }
+//     }
+//   }
+
+//   // Update active
+//   if (clicked_button_name == "Next" && active_page < maximum_pages) {
+//     console.log("active index: " + active_index);
+//     active_button.classList.remove("active");
+
+//     // var new_button = document.querySelectorAll(
+//     //   '[class*="index' + (active_index + 1) + '"]'
+//     // )[0];
+//     console.log("index" + (active_index + 1));
+//     console.log(
+//       document.getElementsByClassName("index" + (active_index + 1))
+//     );
+
+//     // page_to_display = active_page + 1;
+//   }
+
+// new_button.classList.add("active");
+
+// if (active_page != clicked_button_name) {
+//   // Actions for previous and next buttons & Navigate to page
+//   if (["Previous", "Next"].includes(clicked_button_name)) {
+//     // Find out the page to be displayed
+//     if (clicked_button_name == "Previous" && active_page > 1) {
+//       page_to_display = active_page - 1;
+//     } else if (
+//       clicked_button_name == "Next" &&
+//       active_page < (await eel.get_maximum_pages()())
+//     ) {
+//       page_to_display = active_page + 1;
+//     } else {
+//       page_to_display = active_page;
+//     }
+//   } else if (active_page != clicked_button_name) {
+//     page_to_display = clicked_button_name;
+//   }
+
+//   if (clicked_button_name != "...") {
+//     // Remove active from its class list
+//     active_button.classList.remove("active");
+//     // Set the next active page and display articles
+//     var page_to_activate = document.querySelectorAll(
+//       '[class*="page_' + page_to_display + '"]'
+//     )[0];
+//     page_to_activate.classList.add("active");
+//     display_articles(parseInt(page_to_display));
+//   }
+// }
+// }
