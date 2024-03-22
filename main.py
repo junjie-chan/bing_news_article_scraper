573375b7c0785c5c0f682d86c2ba1fbc3e0ee90f
Junjie Chen <junjie.chan@outlook.com>
2024-03-22 23:17:29 +1000

    Fix Bug: correct logic for generating the list of button text in different status.

--- main.py
+++ main.py
@@ -41,25 +41,23 @@ def get_maximum_pages():
 
 
 @expose
-def generate_page_buttons(articles_per_page=ARTICLES_PER_PAGE, starting_page=1):
+def generate_page_buttons(starting_page=1):
     num_of_page = get_maximum_pages()
-    tags = ['Previous', *range(1, num_of_page+1), 'Next'] if num_of_page <= 8 else [
-        'Previous', 1, 2, 3, 4, 5, 'ellipsis', num_of_page-2, num_of_page-1, num_of_page, 'Next']
+    tags = get_buttons_text(starting_page, num_of_page)
     return run_template('next_page_template.jinja', tags)
 
 
 @expose
-def get_buttons_text(index4_value, max_pages):
-    if index4_value < max_pages-6:
-        if index4_value == 3:
-            return ['Previous', *range(1, 6), '...', *range(max_pages-2, max_pages+1), 'Next']
-        else:
-            return ['Previous', *range(index4_value-2, index4_value), index4_value,
-                    *range(index4_value+1, index4_value+3), '...', *
-                    range(max_pages-2, max_pages+1), 'Next']
-    else:
+def get_buttons_text(clicked_page, max_pages):
+    # 最前位置
+    if clicked_page <= 4:
+        return ['Previous', *range(1, 9), '...', 'Next']
+    # 最后位置
+    elif clicked_page >= max_pages-4:
         return ['Previous', '...', *range(max_pages-7, max_pages+1), 'Next']
+    # 其他中间位置
+    else:
+        return ['Previous', '...', *range(clicked_page-3, clicked_page+4), '...', 'Next']
 
 
-# start('index.html', size=WINDOW_SIZE)
-print(get_maximum_pages())
+start('index.html', size=WINDOW_SIZE)
