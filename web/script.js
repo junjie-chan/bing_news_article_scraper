f3be735f45bcd9f29d6a2148a78fa7a286982328
Junjie Chen <junjie.chan@outlook.com>
2024-03-27 13:21:11 +1000

    Code cleaning for .js file.

--- web/script.js
+++ web/script.js
@@ -1,12 +1,3 @@
-// const b = document.getElementById("btn");
-// b.addEventListener("click", (event) => {
-//   p.innerHTML = "button clicked!";
-//   //   eel.to_second_page();
-//   // redirect
-//   //   window.location.href = "test.html";
-//   //   window.location.href = "https://www.google.com";
-// });
-
 // Display Sidebar
 eel.expose(display_sidebar);
 function display_sidebar() {
@@ -48,30 +39,22 @@ function load_url(url) {
   window.open(url, "_blank");
 }
 
-$(document).ready(function () {
-  /* Every time the window is scrolled ... */
-  $(window).scroll(function () {
-    /* Check the location of each desired element */
-    $(".reveal").each(function (i) {
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
+// $(document).ready(function () {
+//   /* Every time the window is scrolled ... */
+//   $(window).scroll(function () {
+//     /* Check the location of each desired element */
+//     $(".reveal").each(function (i) {
+//       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+//       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-      /* If the object is completely visible in the window, fade it in */
-      if (bottom_of_window > bottom_of_object) {
-        $(this).animate({ opacity: "1" }, 1200);
-        /* 1500 is the duration of fade effect */
-      }
-    });
-  });
-});
-
-// Load Content
-display_sidebar();
-display_articles();
-display_next_page_buttons();
-
-// 翻页处理
-var active_button_name = 1; // Default
+//       /* If the object is completely visible in the window, fade it in */
+//       if (bottom_of_window > bottom_of_object) {
+//         $(this).animate({ opacity: "1" }, 1200);
+//         /* 1500 is the duration of fade effect */
+//       }
+//     });
+//   });
+// });
 
 // Get the class name that contains specific string of an element
 function get_element_class_name(element, name) {
@@ -144,6 +127,11 @@ async function move_buttons(clicked_button, maximum_pages) {
 function get_button_text(button) {
   return button.querySelector("a").textContent;
 }
+
+// Load Content
+display_sidebar();
+display_articles();
+display_next_page_buttons();
 
 // Get the page button that was clicked
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -310,52 +298,3 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   });
 });
-
-//   // Update active
-//   if (clicked_button_name == "Next" && active_page < maximum_pages) {
-//     console.log("active index: " + active_index);
-//     active_button.classList.remove("active");
-
-//     // var new_button = document.querySelectorAll(
-//     //   '[class*="index' + (active_index + 1) + '"]'
-//     // )[0];
-//     console.log("index" + (active_index + 1));
-//     console.log(
-//       document.getElementsByClassName("index" + (active_index + 1))
-//     );
-
-//     // page_to_display = active_page + 1;
-//   }
-
-// new_button.classList.add("active");
-
-// if (active_page != clicked_button_name) {
-//   // Actions for previous and next buttons & Navigate to page
-//   if (["Previous", "Next"].includes(clicked_button_name)) {
-//     // Find out the page to be displayed
-//     if (clicked_button_name == "Previous" && active_page > 1) {
-//       page_to_display = active_page - 1;
-//     } else if (
-//       clicked_button_name == "Next" &&
-//       active_page < (await eel.get_maximum_pages()())
-//     ) {
-//       page_to_display = active_page + 1;
-//     } else {
-//       page_to_display = active_page;
-//     }
-//   } else if (active_page != clicked_button_name) {
-//     page_to_display = clicked_button_name;
-//   }
-
-//   if (clicked_button_name != "...") {
-//     // Remove active from its class list
-//     active_button.classList.remove("active");
-//     // Set the next active page and display articles
-//     var page_to_activate = document.querySelectorAll(
-//       '[class*="page_' + page_to_display + '"]'
-//     )[0];
-//     page_to_activate.classList.add("active");
-//     display_articles(parseInt(page_to_display));
-//   }
-// }
-// }
